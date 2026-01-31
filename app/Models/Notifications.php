6e78547b1ca2642597e7fa4fa5bff7506984f53d
Ace <akhmadfauzy40@gmail.com>
2026-01-31 15:04:34 +0700

    setup: model

--- app/Models/Notifications.php
+++ app/Models/Notifications.php
@@ -2,10 +2,26 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\HasUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Notifications extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuid;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'message',
+        'is_read',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
